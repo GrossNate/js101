b191ca977f2f31f636cbba1b03d40477c6618369
Nathan Gross <nathan.gross@gmail.com>
2022-12-18 20:03:39 +0800

    Adding readline-sync and initial few lines.

---
+++ lesson_2/calculator.js
@@ -0,0 +1,12 @@
+// Ask the user for the first number.
+// Ask the user for the second number.
+// Ask the user for an operation to perform.
+// Perform the operation on the two numbers.
+// Print the result to the terminal.
+
+const readline = require('readline-sync');
+
+console.log('Welcome to Calculator!');
+
+console.log("What's the first number?");
+let number1 = readline.question();
