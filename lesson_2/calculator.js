6c1d42bc23eeb00f504747fa9f430dc12525b191
Nathan Gross <nathan.gross@gmail.com>
2023-02-10 10:52:36 +0800

    Adding ability to ask user for another calculation.

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -13,45 +13,61 @@ function prompt(message) {
 function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
+
+function calculate() {
+  prompt("What's the first number?");
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number1 = readline.question();
+  }
+
+  prompt("What's the second number?");
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number2 = readline.question();
+  }
+
+  prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt('Must choose 1, 2, 3, or 4');
+    operation = readline.question();
+  }
+  let output;
+  switch (operation) {
+    case '1':  // '1' represents addition
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':  // '2' represents subtraction
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':  // '3' represents multiplication
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':  // '4' represents division
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`The result is: ${output}`);
+}
+
+let running = true;
 prompt('Welcome to Calculator!');
 
-prompt("What's the first number?");
-let number1 = readline.question();
-
-while (invalidNumber(number1)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number1 = readline.question();
+while (running) {
+  calculate();
+  prompt('Would you like to perform another calculation? (Y/N)');
+  let runAgain = readline.question();
+  while (!['Y', 'N', 'y', 'n'].includes(runAgain)) {
+    prompt('Answer must be Y or N. Would you like to perform another calculation?');
+    runAgain = readline.question();
+  }
+ 
+  running = (runAgain.toLowerCase() === 'y');
 }
-
-prompt("What's the second number?");
-let number2 = readline.question();
-
-while (invalidNumber(number2)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number2 = readline.question();
-}
-
-prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3, or 4');
-  operation = readline.question();
-}
-let output;
-switch (operation) {
-  case '1':  // '1' represents addition
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':  // '2' represents subtraction
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':  // '3' represents multiplication
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':  // '4' represents division
-    output = Number(number1) / Number(number2);
-    break;
-}
-
-prompt(`The result is: ${output}`);
