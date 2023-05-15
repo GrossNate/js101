abc75fc9fa1a3509140211b1fccc0ce579c5b9bd
Nathan Gross <nathan.gross@gmail.com>
2023-05-15 10:19:24 -0500

    Refactored, improved validation, improved prompts.

--- lesson_2/mortgage-calculator.js
+++ lesson_2/mortgage-calculator.js
@@ -1,6 +1,16 @@
 const readline = require('readline-sync');
-const usdFormatter = Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
-const percentFormatter = Intl.NumberFormat('en-US', {style: 'percent', minimumFractionDigits: 2, trailingZeroDisplay: 'stripIfInteger'});
+const formatUsd = Intl.NumberFormat(
+  'en-US',
+  {style: 'currency', currency: 'USD'}
+).format;
+const formatPercent = Intl.NumberFormat(
+  'en-US',
+  {
+    style: 'percent',
+    minimumFractionDigits: 2,
+    trailingZeroDisplay: 'stripIfInteger'
+  }
+).format;
 
 /**
  * @typedef {Object} PaymentPlan
@@ -25,6 +35,10 @@ function truncDecimal(number, digits) {
     truncatedValue += Math.pow(10, -digits);
   }
   return truncatedValue;
+}
+
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
 
 /**
@@ -61,6 +75,30 @@ function generatePaymentPlan(principle, apr, period) {
 }
 
 /**
+ * Creates a payment plan description that should be human-readable.
+ * @param {PaymentPlan} paymentPlan - details of the payment plan.
+ * @param {number} principle
+ * @param {number} apr
+ * @param {number} period
+ * @returns {String}
+ */
+function generateRepaymentDescription(paymentPlan, principle, apr, period) {
+  let repaymentDescription = '';
+  if (paymentPlan.numberIdenticalPayments === 0 && period === 1) {
+    repaymentDescription = `Repaying this loan of ${formatUsd(principle)} at `
+      + `${formatPercent(apr)} will require a single monthly payment of `
+      + `${formatUsd(paymentPlan.finalPaymentAmount)}`;
+  } else {
+    repaymentDescription = `Repaying this loan of ${formatUsd(principle)} at `
+      + `${formatPercent(apr)} will require `
+      + `${paymentPlan.numberIdenticalPayments} monthly payments of `
+      + `${formatUsd(paymentPlan.identicalPaymentAmount)} and a final `
+      + `payment of ${formatUsd(paymentPlan.finalPaymentAmount)}`;
+  }
+  return repaymentDescription;
+}
+
+/**
  * Parses the text entered into a number of months and ensures it's > 0.
  * @param {String} periodText
  * @returns {number} number of months in period or NaN if unparseable.
@@ -85,7 +123,9 @@ function parseAndValidatePeriod(periodText) {
  * @returns {number} The principle as a number or NaN if it can't parse.
  */
 function parseAndValidatePrinciple(principleText) {
-  const returnPrinciple = Number.parseFloat(principleText.replaceAll(/[^\d^.^-]/g, ''));
+  const returnPrinciple = Number.parseFloat(
+    principleText.replaceAll(/[^\d^.^-]/g, '')
+  );
   return (returnPrinciple > 0 ? returnPrinciple : NaN);
 }
 
@@ -102,31 +142,84 @@ function parseAndValidateRate(rateText) {
   return (returnRate > 0 ? returnRate : NaN);
 }
 
-function prompt(message) {
-  console.log(`=> ${message}`);
+/**
+ * Gets the principle amount as input from user.
+ * @returns {number} The amount of the loan.
+ */
+function getPrinciple() {
+  prompt('Please enter the loan principle amount:');
+  let principle = parseAndValidatePrinciple(readline.question());
+  while (Number.isNaN(principle)) {
+    prompt("I can't parse that amount. Examples of valid input are "
+    + "'$900,000.23' or '900000.23'. Try again:");
+    principle = parseAndValidatePrinciple(readline.question());
+  }
+  console.log("\n");
+  return principle;
 }
 
-// Actual program body.
-prompt('Please enter the loan principle amount:');
-let principle = parseAndValidatePrinciple(readline.question());
-while (Number.isNaN(principle)) {
-  prompt("I can't parse that amount. Examples of valid input are '$900,000.23' or '900000.23'. Try again:");
-  principle = parseAndValidatePrinciple(readline.question());
+/**
+ * Gets the APR from the user. Note it's an APR, not APY.
+ * @returns {number} The annual percentage rate.
+ */
+function getApr() {
+  prompt('Please enter annual percentage rate:');
+  let apr = parseAndValidateRate(readline.question());
+  while (Number.isNaN(apr)) {
+    prompt("I can't parse that rate. Examples of valid input are '.055' or "
+      + "'5.5%'. Try again:");
+    apr = parseAndValidateRate(readline.question());
+  }
+  console.log("\n");
+  return apr;
 }
 
-prompt('Please enter annual percentage rate:');
-let apr = parseAndValidateRate(readline.question());
-while (Number.isNaN(apr)) {
-  prompt("I can't parse that rate. Examples of valid input are '.055' or '5.5%'. Try again:");
-  apr = parseAndValidateRate(readline.question());
+/**
+ * Gets the repayment period from the user.
+ * @returns {number} The number of months in the repayment period.
+ */
+function getPeriod() {
+  prompt('Please enter the repayment period (you can denote years with "y" and '
+  + 'months with "m", e.g. "5y 6m" is interpreted as 66 months; plain numeric '
+  + 'values are interpreted as months):');
+  let period = parseAndValidatePeriod(readline.question());
+  while (Number.isNaN(period)) {
+    prompt("I can't parse that as a loan period. Examples of valid input are "
+    + "'4y 6m' or '54'. Also, the period must be greater than 0. Try again:");
+    period = parseAndValidatePeriod(readline.question());
+  }
+  console.log("\n");
+  return period;
 }
 
-prompt('Please enter the repayment period (you can denote years with "y" and months with "m"; plain numeric values are interpreted as months):');
-let period = parseAndValidatePeriod(readline.question());
-while (Number.isNaN(period)) {
-  prompt("I can't parse that as a loan period. Examples of valid input are '4y 6m' or '54'. Also, the period must be greater than 0. Try again:");
-  period = parseAndValidatePeriod(readline.question());
+function runCalculator() {
+  let principle = getPrinciple();
+  let apr = getApr();
+  let period = getPeriod();
+
+  let paymentPlan = generatePaymentPlan(principle, apr, period);
+
+  let repaymentDescription = generateRepaymentDescription(
+    paymentPlan,
+    principle,
+    apr,
+    period);
+
+  console.log(repaymentDescription + "\n");
 }
 
-let paymentPlan = generatePaymentPlan(principle, apr, period);
-console.log(`Repaying this loan of ${usdFormatter.format(principle)} at ${percentFormatter.format(apr)} will require ${paymentPlan.numberIdenticalPayments} monthly payments of ${usdFormatter.format(paymentPlan.identicalPaymentAmount)} and a final payment of ${usdFormatter.format(paymentPlan.finalPaymentAmount)}`);
+// Actual body of program.
+while (true) {
+  console.clear();
+  console.log("Welcome to the Fairly Forgiving Mortgage Calculator!\n\n"
+    + "It's called 'Fairly Forgiving' because you can enter values in ways "
+    + "that should make intuitive sense. For example, .055 and 5.5% are "
+    + "interpreted the same. (But 5.5 would be interpreted as 550%, so do be "
+    + "careful!)\n\n"
+    + "Note also that this can only be used to compute loans that have a "
+    + "positive interest rate and have at least a one month repayment period. "
+    + "\n");
+  runCalculator();
+  prompt('Would you like to run another calculation? (Y/N):');
+  if (readline.question().toLowerCase() !== 'y') break;
+}
